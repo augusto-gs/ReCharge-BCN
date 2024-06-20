a8d36ee4efc102557b3343167f2b483be111959d
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-20 21:08:08 +0200

    Add favicon and description to index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/app";
 import mainTheme from "../styles/mainTheme";
 import { ThemeProvider } from "styled-components";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
