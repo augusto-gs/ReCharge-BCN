b9727185c78a9041f2b0a1bdf08fd0fc078141de
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-21 22:57:47 +0200

    Remove img element from Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,27 +1,23 @@
 import Button from "../Button/Button";
+import HeroStyled from "./HeroStyled";
 
 const Hero = () => (
-  <section className="hero">
-    <img
-      src="/images/bike_images.jpg"
-      alt="Electric bikes parked on the street on paralel"
-      className="hero__image"
-      width="1444px"
-      height="558px"
-    />
-    <h1 className="hero__title">Find your motorbike</h1>
-    <p className="hero__introduction">
-      Find the nearest available charging points for your electric motorbike
-      effortlessly. Start your journey with a full charge today!
-    </p>
-    <Button
-      actionOnClick={() => {}}
-      type={"button"}
-      text={"Map"}
-      modifier={"button--hero"}
-      isDisabled={false}
-    />
-  </section>
+  <HeroStyled className="hero">
+    <div className="hero__layout">
+      <h1 className="hero__title">Find your motorbike</h1>
+      <p className="hero__introduction">
+        Find the nearest available charging points for your electric motorbike
+        effortlessly. Start your journey with a full charge today!
+      </p>
+      <Button
+        actionOnClick={() => {}}
+        type={"button"}
+        text={"Map"}
+        modifier={"button--hero"}
+        isDisabled={false}
+      />
+    </div>
+  </HeroStyled>
 );
 
 export default Hero;
