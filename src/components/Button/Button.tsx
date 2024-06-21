cb2c9fa2ab7d6468f64187585eacc23c4636fdee
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-21 17:09:55 +0200

    Add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+interface ButtonProps {
+  actionOnClick: () => void;
+  type: "submit" | "button";
+  text: string;
+  modifier: "button--hero";
+  isDisabled: boolean;
+}
+
+const Button = ({
+  text,
+  actionOnClick,
+  isDisabled,
+  modifier,
+  type,
+}: ButtonProps) => {
+  <button
+    className={`button ${modifier}`}
+    onClick={actionOnClick}
+    disabled={isDisabled}
+    type={type}
+  >
+    {text}
+  </button>;
+};
+
+export default Button;
