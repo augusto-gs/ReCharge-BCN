5b00e08dab258bd464c820365ac2163ef75b155f
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-22 12:43:59 +0200

    Add testing suite for Table functionality in App component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -3,9 +3,19 @@ import { customRender } from "../../testUtils/testUtils";
 import App from "./App";
 
 describe("Given an App component", () => {
-  describe("When it is rendered on screen", () => {
+  describe("When it is rendered on screen on the TablePage", () => {
     test("Then you should see a title 'Find your motorbike' on a heading", () => {
       const titleText = "Find your motorbike";
+
+      customRender(<App />, true);
+
+      const title = screen.getByRole("heading", { name: titleText });
+
+      expect(title).toBeInTheDocument();
+    });
+
+    test("Then you should see a title 'Charging points' on a heading", () => {
+      const titleText = "Charging points";
 
       customRender(<App />, true);
 
