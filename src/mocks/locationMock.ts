4af92f9dd4720f0c2b7697317b156250405f4bfb
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-23 18:27:03 +0200

    Add testing suite for motorbikeReducer

--- src/mocks/locationMock.ts
+++ src/mocks/locationMock.ts
@@ -1,5 +1,4 @@
 import locations from "../data/bikeData";
 
-const locationMock = locations[0];
-
-export default locationMock;
+export const locationMock = locations[0];
+export const locationsMock = locations;
