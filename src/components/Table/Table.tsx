c43d4ef979eeba74d795782febff55ee48e3f0b0
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-24 18:51:23 +0200

    Add conection to database on table page and component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,21 +1,27 @@
-import locations from "../../data/bikeData";
+import { useAppSelector } from "../../store/hooks";
 import LocationCard from "../LocationCard/LocationCard";
 import TableStyled from "./TableStyled";
 
-const Table = () => (
-  <TableStyled className="charging-points">
-    <h2 className="charging-points__title">Charging points</h2>
-    <p className="charging-points__introduction">
-      Look for the different charging ports available in the city{" "}
-    </p>
-    <ul className="table">
-      {locations.map((location) => (
-        <li className="table__item" key={location.id}>
-          {<LocationCard location={location} />}
-        </li>
-      ))}
-    </ul>
-  </TableStyled>
-);
+const Table = () => {
+  const { locations } = useAppSelector(
+    (state) => state.motorbikesLocationState,
+  );
+
+  return (
+    <TableStyled className="charging-points">
+      <h2 className="charging-points__title">Charging points</h2>
+      <p className="charging-points__introduction">
+        Look for the different charging ports available in the city{" "}
+      </p>
+      <ul className="table">
+        {locations.map((location) => (
+          <li className="table__item" key={location.id}>
+            {<LocationCard location={location} />}
+          </li>
+        ))}
+      </ul>
+    </TableStyled>
+  );
+};
 
 export default Table;
