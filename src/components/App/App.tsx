7f251de415bfe47a99d1451bdc7216ce40f77f13
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-25 12:00:32 +0200

    Add toast styles to GlobalStyles and showToast.ts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,12 @@ import Header from "../Header/Header";
 import ContainerStyled from "../../../styles/shared/ContainerStyled";
 import { Routes, Navigate, Route } from "react-router-dom";
 import TablePage from "../../Pages/TablePage/TablePage";
-import { ToastContainer } from "react-toastify";
+import ToastStyled from "../../../styles/shared/ToastStyled";
 import "react-toastify/dist/ReactToastify.min.css";
 
 const App = (): React.ReactElement => (
   <ContainerStyled className="main-container">
-    <ToastContainer />
+    <ToastStyled />
     <Header />
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
--- src/utils/showToast.ts
+++ src/utils/showToast.ts
@@ -1,16 +1,24 @@
-import { toast } from "react-toastify";
+import { toast, ToastOptions } from "react-toastify";
 
 const showToast = (
   type: "success" | "error",
   message: string,
-  options?: object,
+  options?: ToastOptions,
 ) => {
   switch (type) {
     case "success":
-      toast.success(message, options);
+      toast.success(message, {
+        style: { background: "#55b938" },
+        progressStyle: { backgroundColor: "#ccEAc4" },
+        ...options,
+      });
       break;
     case "error":
-      toast.error(message, options);
+      toast.error(message, {
+        style: { background: "#d65745" },
+        progressStyle: { backgroundColor: "#F3CDC8" },
+        ...options,
+      });
       break;
   }
 };
