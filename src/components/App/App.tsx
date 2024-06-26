0ebba1a47772c968b6f289cd87327037c54f2b2c
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-26 13:42:05 +0200

    Add MapPage component and its styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import Header from "../Header/Header";
 import ContainerStyled from "../../../styles/shared/ContainerStyled";
 import { Routes, Navigate, Route } from "react-router-dom";
-import TablePage from "../../Pages/TablePage/TablePage";
+import TablePage from "../../pages/TablePage/TablePage";
 import ToastStyled from "../../../styles/shared/ToastStyled";
 import "react-toastify/dist/ReactToastify.min.css";
+import MapPage from "../../pages/MapPage/MapPage";
 
 const App = (): React.ReactElement => (
   <ContainerStyled className="main-container">
@@ -12,6 +13,7 @@ const App = (): React.ReactElement => (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<TablePage />} />
+      <Route path="/map" element={<MapPage />} />
     </Routes>
   </ContainerStyled>
 );
