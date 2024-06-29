738f85fdc3b27b1668874a381b6ea14cde570159
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-29 23:29:46 +0200

    Refactor test suites to include map context

--- src/components/MapDisplay/MapDisplay.tsx
+++ src/components/MapDisplay/MapDisplay.tsx
@@ -1,10 +1,10 @@
 import "ol/ol.css";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
-import { fromLonLat, toLonLat } from "ol/proj";
+import { fromLonLat } from "ol/proj";
 import VectorSource from "ol/source/Vector";
 import Vector from "ol/layer/Vector";
 import MapDisplayStyled from "./MapDisplayStyled";
@@ -13,21 +13,8 @@ import useMotorbikeLocationApi from "../../hooks/useMotorbikeLocationApi/useMoto
 import hasAvailableLocations from "../../utils/hasAvailableLocations";
 import useMapLocations from "../../hooks/useMapLocations/useMapLocations";
 import removeCarLocationChargers from "../../utils/removeCarLocationChargers";
-import Overlay from "ol/Overlay";
-import Button from "../Button/Button";
-import { Point } from "ol/geom";
-import Feature from "ol/Feature";
-import Style from "ol/style/Style";
-import Icon from "ol/style/Icon";
-import Overlay from "ol/Overlay";
-import Button from "../Button/Button";
-import { Point } from "ol/geom";
-import Feature from "ol/Feature";
-import Style from "ol/style/Style";
-import Icon from "ol/style/Icon";
 
 const MapDisplay = () => {
-  const mapContainerRef = useRef(null);
   const { getBikeLocations } = useMotorbikeLocationApi();
 
   const { locations } = useAppSelector(
@@ -45,15 +32,6 @@ const MapDisplay = () => {
   useEffect(() => {
     const source = new VectorSource({
       wrapX: false,
-    });
-
-    const overlay = new Overlay({
-      element: mapContainerRef.current!,
-      autoPan: {
-        animation: {
-          duration: 250,
-        },
-      },
     });
 
     const vector = new Vector({
@@ -75,58 +53,19 @@ const MapDisplay = () => {
         maxZoom: 18,
         minZoom: 8,
       }),
-      overlays: [overlay],
     });
-
-    const testPoint = new Point(fromLonLat([2.1734, 41.3851]));
-    const testFeature = new Feature(testPoint);
-
-    testFeature.setStyle(
-      new Style({
-        image: new Icon({
-          anchor: [0, 1],
-          src: "/images/phone-icon.svg",
-        }),
-      }),
-    );
-
-    source.addFeature(testFeature);
 
     source.addFeatures(hasMapLocations());
-
-    map.on("click", (event) => {
-      map.forEachFeatureAtPixel(event.pixel, (clickedFeature) => {
-        if (clickedFeature) {
-          const coordinate = event.coordinate;
-          const featureCoordinate =
-            clickedFeature.getProperties().geometry.flatCoordinates;
-
-          const realCoordinate = toLonLat(featureCoordinate);
-
-          overlay.setPosition(coordinate);
-          console.log("Feature clicked:", clickedFeature.get("name"));
-        }
-      });
-    });
 
     return () => map.setTarget();
   }, [hasMapLocations, dispatch, getBikeLocations, locations]);
 
   return (
-    <MapDisplayStyled data-testid="map" id="map" tabIndex={0}>
-      <div
-        ref={mapContainerRef}
-        style={{ width: "40px", height: "40px" }}
-        id="popup"
-        className="ol-popup"
-      >
-        <Button />
-        <a href="hola" id="popup-closer" className="ol-popup-closer">
-          hola
-        </a>
-        <div id="popup-content"></div>
-      </div>
-    </MapDisplayStyled>
+    <MapDisplayStyled
+      data-testid="map"
+      id="map"
+      tabIndex={0}
+    ></MapDisplayStyled>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,18 @@ import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import MapProviderWrapper from "./store/features/map/MapProviderWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={mainTheme}>
-        <Provider store={store}>
-          <GlobalStyle />
-          <App />
-        </Provider>
+        <MapProviderWrapper>
+          <Provider store={store}>
+            <GlobalStyle />
+            <App />
+          </Provider>
+        </MapProviderWrapper>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
