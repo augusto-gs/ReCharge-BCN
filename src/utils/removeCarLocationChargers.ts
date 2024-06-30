46bac0864bb0ca986636310a759775adb405150b
Augusto Gomez <agomezsantos95@gmail.com>
2024-06-30 11:47:01 +0200

    Refactor project utilities to better filter locations

--- src/utils/removeCarLocationChargers.ts
+++ src/utils/removeCarLocationChargers.ts
@@ -1,12 +1,17 @@
 import { ChargingLocationStructure } from "../types";
 
-const removeCarLocationChargers = (locations: ChargingLocationStructure[]) =>
-  locations.map((location) => ({
+const removeCarLocationChargers = (locations: ChargingLocationStructure[]) => {
+  const filteredLocations = locations.map((location) => ({
     ...location,
     stations: location.stations.map((station) => ({
       ...station,
-      ports: station.ports.filter((port) => port.notes !== ""),
+      ports: station.ports.filter((port) => port.notes === "MOTORCYCLE_ONLY"),
     })),
   }));
+
+  return filteredLocations.filter((location) =>
+    location.stations.some((station) => station.ports.length > 0),
+  );
+};
 
 export default removeCarLocationChargers;
