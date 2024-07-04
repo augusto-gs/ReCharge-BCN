836a0cf2cad3524e37892e3feb7f0b1c3cf788b2
Augusto Gomez <agomezsantos95@gmail.com>
2024-07-04 16:51:46 +0200

    Refactor useMap custom hook and map context

--- src/store/features/map/MapProviderWrapper.tsx
+++ src/store/features/map/MapProviderWrapper.tsx
@@ -5,6 +5,7 @@ import Vector from "ol/layer/Vector";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import Map from "ol/Map";
+import Overlay from "ol/Overlay";
 
 const MapProviderWrapper = ({
   children,
@@ -24,8 +25,18 @@ const MapProviderWrapper = ({
       }),
     });
 
+    const overlay = new Overlay({
+      autoPan: {
+        animation: {
+          duration: 250,
+        },
+      },
+      id: "popup",
+    });
+
     const map = new Map({
       layers: [tileLayer, vector],
+      overlays: [overlay],
     });
 
     return { map, vector, source };
